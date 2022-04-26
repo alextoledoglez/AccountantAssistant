f8261a4d00bfe50b13d7edc030c10b75af6c791c
Alejandro Toledo González <alejandrotg146@gmail.com>
2022-04-25 21:55:25 -0400

    Improving home view model flows

--- app/src/main/java/com/personal/accountantAssistant/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/personal/accountantAssistant/ui/home/HomeViewModel.kt
@@ -13,7 +13,8 @@ import com.personal.accountantAssistant.domain.useCases.GetLastDateUseCase
 import com.personal.accountantAssistant.domain.useCases.SetPeriodDatesUseCase
 import com.personal.accountantAssistant.extensions.*
 import com.personal.accountantAssistant.providers.AnalyticsProvider
-import kotlinx.coroutines.flow.singleOrNull
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.launch
 import java.math.BigDecimal
 import java.util.*
@@ -24,15 +25,15 @@ class HomeViewModel(
     val getLastDate: GetLastDateUseCase,
     val setPeriodDates: SetPeriodDatesUseCase,
     val getAvailableMoney: GetAvailableMoneyUseCase,
-    private val buysRepository: BuysRepository?,
-    private val billsRepository: BillsRepository?
+    private val buysRepository: BuysRepository,
+    private val billsRepository: BillsRepository
 ) : BaseViewModel(analytics) {
 
-    private val _dashboardValues = MutableLiveData<List<DashboardItemModel>>()
-    val dashboardValues = _dashboardValues
+    private val _firstDate = MutableLiveData<Date?>()
+    private val firstDate = _firstDate
 
-    private val _expensesValues = MutableLiveData<ExpensesValuesModel>()
-    val expensesValues = _expensesValues
+    private val _lastDate = MutableLiveData<Date?>()
+    private val lastDate = _lastDate
 
     private val _periodValue = MutableLiveData<String>()
     val periodValue: LiveData<String> get() = _periodValue
@@ -40,11 +41,11 @@ class HomeViewModel(
     private val _availableMoney = MutableLiveData<BigDecimal>()
     val availableMoney: LiveData<BigDecimal> get() = _availableMoney
 
-    private val _firstPeriodDate = MutableLiveData<Date?>()
-    private val firstPeriodDate = _firstPeriodDate
+    private val _expensesValues = MutableLiveData<ExpensesValuesModel>()
+    val expensesValues = _expensesValues
 
-    private val _lastPeriodDate = MutableLiveData<Date?>()
-    private val lastPeriodDate = _lastPeriodDate
+    private val _dashboardValues = MutableLiveData<List<DashboardItemModel>>()
+    val dashboardValues = _dashboardValues
 
     fun isZeroLessThan(value: BigDecimal) = (value >= BigDecimal.ZERO)
 
@@ -52,36 +53,49 @@ class HomeViewModel(
 
     fun isExpensesMoreThanAvailable(value: BigDecimal) = !isExpensesLessThanAvailable(value)
 
-    fun calculateExpenses() = launch {
-        setLoading()
-        val lastDate = getLastDate().singleOrNull()
-        setPeriodDates(getFirstDate().singleOrNull(), lastDate)
-        val buysExpenses = buysRepository?.getTotalValueUntil(lastDate)
-            ?.onError { setMessage(it.message) }
-            ?.singleOrNull().orZero()
-        val billsExpenses = billsRepository?.getTotalValueUntil(lastDate)
-            ?.onError { setMessage(it.message) }
-            ?.singleOrNull().orZero()
-        val totalExpenses = buysExpenses.plus(billsExpenses)
-        _availableMoney.postValue(getAvailableMoney().singleOrNull())
-        _expensesValues.postValue(ExpensesValuesModel(buysExpenses, billsExpenses, totalExpenses))
-        setData()
+    fun calculateExpenses() {
+        launch {
+            setLoading()
+
+            combine(getFirstDate(), getLastDate()) { init, end ->
+                val period = "${init.toDateStr()}${String.DASH_SEPARATOR}${end.toDateStr()}"
+                _firstDate.postValue(init)
+                _lastDate.postValue(end)
+                _periodValue.postValue(period)
+            }.onError { setMessage(it.message) }.collect()
+
+            getAvailableMoney()
+                .onError { setMessage(it.message) }
+                .collect { _availableMoney.postValue(it) }
+
+            combine(
+                buysRepository.getTotalValueUntil(lastDate.value),
+                billsRepository.getTotalValueUntil(lastDate.value)
+            ) { buys, bills ->
+                val total = buys?.plus(bills.orZero())
+                val expenses = ExpensesValuesModel(buys, bills, total)
+                _expensesValues.postValue(expenses)
+            }.onError { setMessage(it.message) }.collect()
+
+            setData()
+        }
     }
 
     fun getSelectedPeriod() = androidx.core.util.Pair(
-        firstPeriodDate.value?.time.toUtcTime(), lastPeriodDate.value?.time.toUtcTime()
+        firstDate.value?.time.toUtcTime(), lastDate.value?.time.toUtcTime()
     )
 
     fun savePeriodDates(period: androidx.core.util.Pair<Long, Long>?) {
-        period?.let { savePeriodDates(it.first.toUtcDate(), it.second.toUtcDate()) }
-    }
-
-    private fun savePeriodDates(firstDate: Date?, lastDate: Date?) {
-        _firstPeriodDate.postValue(firstDate)
-        _lastPeriodDate.postValue(lastDate)
-        val period = "${firstDate.toDateStr()}${String.DASH_SEPARATOR}${lastDate.toDateStr()}"
-        _periodValue.postValue(period)
-        setPeriodDates(firstDate, lastDate)
+        period?.let {
+            val init = it.first.toUtcDate()
+            val end = it.second.toUtcDate()
+            launch {
+                setPeriodDates(init, end).collect {
+                    _firstDate.postValue(init)
+                    _lastDate.postValue(end)
+                }
+            }
+        }
     }
 
     fun postDashboardValues(list: List<DashboardItemModel>) {
